bbd0462843baf2b06e2582f27363abc381b7c893
Lpdts <Lpdts@qq.com>
2017-10-23 00:58:15 +0800

    update poly2.cpp

--- poly2.cpp
+++ poly2.cpp
@@ -5,6 +5,11 @@
 #define LEN 2048   // 数组strLine 的长度
 #define VAR_LEN 8  // 变量的长度
 
+typedef struct _POLYS {
+    char poly[LEN];
+    struct _POLYS *next;
+}POLYS;
+
 void delBlank(char* a) {
     int i = 0, j = 0;
     while(*(a + i) != '\0') {
@@ -17,6 +22,11 @@ void delBlank(char* a) {
         }
     }
     *(a + j) = '\0';
+}
+
+// 从字符串*poly中
+void delStr(char *poly, char* str){
+
 }
 
 void delVar(char* poly, char* var) {
@@ -77,18 +87,24 @@ int findVar(char* poly, char* var) {
 }
 
 // 找到形如 xy = 0 的项
-int findVar2(char* poly, char * var) {
-    int i = 0, left = 0;
-    int numOfPlusSub = 0;
+int findVar2(char* poly, char* item) {
+    int i = 0, j = 0, left = 0;  // xy=0项的左侧
+    int numOfPlusSub = 0;  // +,-的个数，去掉第一个后个数为零则为xy=0的项
 
     while(*(poly + i) != '\0') {
         if((*(poly + i) == '+' || *(poly + i) == '-') && i != 0 && *(poly + i - 1) != '=') {
             numOfPlusSub++;
         }
-        if(*(poly + i) == '=') {
+        if(*(poly + i) == '=' && *(poly + i - 1) != '=') { // 去掉==的情况
             if(numOfPlusSub == 0) {
-                //todo
+                for(j = 0; left + j < i; j++) {
+                    *(item + j) = *(poly + left + j);
+                }
+                *(item + j) = '\0';
                 return 1;
+            }else {
+                numOfPlusSub = 0;
+                left = i + 1;
             }
         }
         i++;
@@ -98,32 +114,58 @@ int findVar2(char* poly, char * var) {
 
 int main()
 {
-    char polyFileName[] = "C:\\Users\\zhangshutao\\Desktop\\MathematicaCode\\poly.txt";
+    char polyFileName[] = "poly.txt";
 
     FILE* pFile;
-    char strLine[LEN];
+    //char strLine[LEN];
+    POLYS polys;
+    polys.next = NULL;
 
     if((pFile = fopen(polyFileName, "r")) == NULL) {
         printf("无法打开文件");
         return -1;
     }
 
-    fgets(strLine, LEN, pFile);
+    fgets(polys.poly, LEN, pFile);
     //printf("%s\n", strLine);
 
-    int len = strlen(strLine);   // 字符串strLine 的长度
+    int len = strlen(polys.poly);   // 字符串strLine 的长度
 
     // 删除字符串的空格
-    delBlank(strLine);
+    delBlank(polys.poly);
 
-    //printf("%s\n", strLine);
+    printf("%s\n", polys.poly);
 
     char var[VAR_LEN];  // 变量数组
 
-    while(findVar(strLine, var)) {
-        delVar(strLine, var);
-        delBlank(strLine);
+    while(findVar(polys.poly, var)) {
+        delVar(polys.poly, var);
+        delBlank(polys.poly);
         printf("%s]=0\n", var);
     }
-    printf("%s\n", strLine);
+    printf("%s\n", polys.poly);
+
+
+    char item[100];
+    if(findVar2(polys.poly, item)){
+        printf("%s\n", item);
+        int i = 0, j = 0;
+        while(*(item + i) != '\0') {
+            if(*(item + i) == '[') {
+                for(j = 0; j <= 5; j++) {
+                    *(var + j) = *(item + i + j);
+                }
+                *(var + j) = '\0';
+                printf("%s\n", var);
+                POLYS newPolys;
+                newPolys.next = NULL;
+                strcpy(newPolys.poly, polys.poly);
+                delVar(newPolys.poly, var);
+                printf("%s\n", newPolys.poly);
+            }
+            i++;
+        }
+    }
+
+
 }
